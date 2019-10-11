083f360e03f80eb964e13bb0d001527ec3300976
Ali Coşkun <alicoskun@outlook.com>
2019-10-11 14:33:43 +0300

    added docker files

---
+++ GitlabCICD.Test/SampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitlabCICD.Test
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
