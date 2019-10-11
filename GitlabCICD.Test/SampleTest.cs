323b92b991ffa731224e076bf1a004cdea2d910d
Ali Coşkun <alicoskun@outlook.com>
2019-10-11 14:52:30 +0300

    test false

--- GitlabCICD.Test/SampleTest.cs
+++ GitlabCICD.Test/SampleTest.cs
@@ -8,7 +8,7 @@ namespace GitlabCICD.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
